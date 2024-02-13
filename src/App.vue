e26cf7008e4892b4eed10c2ba9a2c0717d7e84d8
Neordan <melhian.adam@gmail.com>
2024-02-13 21:42:20 +0100

    Project Vue JS initialisation

---
+++ src/App.vue
@@ -0,0 +1,41 @@
+<script setup>
+import ProductVue from './components/Products.vue';
+import Header from './components/Header.vue';
+import Register from './components/Register.vue';
+
+</script>
+
+<template>
+  <Header />
+  <ProductVue />
+  <Register />
+</template>
+
+<style scoped>
+header {
+  line-height: 1.5;
+}
+
+.logo {
+  display: block;
+  margin: 0 auto 2rem;
+}
+
+@media (min-width: 1024px) {
+  header {
+    display: flex;
+    place-items: center;
+    padding-right: calc(var(--section-gap) / 2);
+  }
+
+  .logo {
+    margin: 0 2rem 0 0;
+  }
+
+  header .wrapper {
+    display: flex;
+    place-items: flex-start;
+    flex-wrap: wrap;
+  }
+}
+</style>
