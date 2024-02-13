b9721af5cc0e8cf20d9198e7fe297267c59cc231
Neordan <melhian.adam@gmail.com>
2024-02-13 21:55:22 +0100

    Create Products.vue
    File where the list of products are defined

---
+++ src/components/Products.vue
@@ -0,0 +1,33 @@
+<!-- Products.vue -->
+
+<template>
+  <section>
+    <h2>{{ products.length }} produits en vente</h2>
+    <section class="products">
+      <Product v-for="product in products" :key="product.id" :product="product" :cheapest-price="cheapestProducts" :product-class="productClass" />
+    </section>
+  </section>
+</template>
+<script>
+import Product from './Product.vue';
+import productsData from '../../data/products.json';
+
+export default {
+  components: {
+    Product
+  },
+  data() {
+    return {
+      products: productsData,
+      productClass: 'product'
+    };
+  },
+  computed: {
+    cheapestProducts() {
+        return this.products.toSorted(function(a, b) {
+          return a.unit_price - b.unit_price;
+        })[0].unit_price;
+    }
+  }
+};
+</script>
