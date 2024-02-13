6270979951388b46d40d84cba4eb21b4a9bddb83
Neordan <melhian.adam@gmail.com>
2024-02-13 22:01:43 +0100

    Create component product.vue
    File that initializes each product

---
+++ src/components/Product.vue
@@ -0,0 +1,33 @@
+<template>
+    <div :class="[{ 'cheapest-product': product.unit_price === cheapestPrice }, productClass]">
+      <h3>{{ product.name }}</h3>
+      <p v-html="product.description"></p>
+      <p>Prix unitaire: {{ product.unit_price }} €</p>
+      <p>Quantité disponible: {{ product.quantity }}</p>
+      <Rating :value="product.rating" />
+    </div>
+  </template>
+  
+  <script>
+  import Rating from './Rating.vue';
+  
+  export default {
+    props: {
+      product: {
+        type: Object,
+        required: true
+      },
+      cheapestPrice: {
+        type: Number,
+        required: true
+      },
+      productClass: {
+        type: String
+      }
+    },
+    components: {
+      Rating
+    }
+  };
+  </script>
+  
