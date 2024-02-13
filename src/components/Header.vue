c88687a657525af5fbe45718f73038383cf61021
Neordan <melhian.adam@gmail.com>
2024-02-13 21:51:59 +0100

    Create Header.vue
    - Menu + logo

---
+++ src/components/Header.vue
@@ -0,0 +1,17 @@
+<!-- fais moi un composent header -->
+
+<template>
+  <header>
+    <div class="wrapper">
+      <nav>
+        <ul>
+          <li><a href="#">Home</a></li>
+          <li><a href="#">About</a></li>
+          <img class="logo" src="../assets/img/logo2.png" alt="Vue Mastery" />
+          <li><a href="#">Services</a></li>
+          <li><a href="#">Contact</a></li>
+        </ul>
+      </nav>
+    </div>
+  </header>
+</template>
