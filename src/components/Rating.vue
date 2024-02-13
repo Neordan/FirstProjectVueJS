83d542473d8e81c49cd024787994f8c04dd8b429
Neordan <melhian.adam@gmail.com>
2024-02-14 00:47:56 +0100

    Styling improvement

--- src/components/Rating.vue
+++ src/components/Rating.vue
@@ -2,13 +2,13 @@
     <div class="rating">
       <span v-for="n in 5" :key="n" class="stars">
         <template v-if="n <= value">
-          <box-icon name='star' type='solid' color="#DEB904" class="star"></box-icon>
+          <box-icon name='star' type='solid' color="#ff9e0c" class="star"></box-icon>
         </template>
         <template v-else-if="n - 0.5 === value">
-          <box-icon name='star-half' type='solid' color="#DEB904" class="star"></box-icon>
+          <box-icon name='star-half' type='solid' color="#ff9e0c" class="star"></box-icon>
         </template>
         <template v-else>
-          <box-icon name='star' color="#DEB904" class="star"></box-icon>
+          <box-icon name='star' color="#ff9e0c" class="star"></box-icon>
         </template>
       </span>
     </div>
