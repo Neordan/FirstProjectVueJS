af04b0f1a808d18aaa46c0cd5efecce10c123cb6
Neordan <melhian.adam@gmail.com>
2024-02-13 22:12:07 +0100

    Create component rating.vue
    Component to manage product ratings with stars

---
+++ src/components/Rating.vue
@@ -0,0 +1,27 @@
+<template>
+    <div class="rating">
+      <span v-for="n in 5" :key="n" class="stars">
+        <template v-if="n <= value">
+          <box-icon name='star' type='solid' color="#DEB904" class="star"></box-icon>
+        </template>
+        <template v-else-if="n - 0.5 === value">
+          <box-icon name='star-half' type='solid' color="#DEB904" class="star"></box-icon>
+        </template>
+        <template v-else>
+          <box-icon name='star' color="#DEB904" class="star"></box-icon>
+        </template>
+      </span>
+    </div>
+  </template>
+  
+  <script>
+  export default {
+    props: {
+      value: {
+        type: Number,
+        default: 0
+      }
+    }
+  };
+  </script>
+  
