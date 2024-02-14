55d442d24b2d0a47194c1db213702e4244c15a27
Neordan <melhian.adam@gmail.com>
2024-02-14 22:19:15 +0100

    Ajout d'un div pour le style

--- src/components/Product.vue
+++ src/components/Product.vue
@@ -1,9 +1,11 @@
 <template>
-    <div :class="[{ 'cheapest-product': product.unit_price === cheapestPrice }, productClass]">
+  <div :class="[{ 'cheapest-product': product.unit_price === cheapestPrice }, productClass]">
+    <div>
       <h3>{{ product.name }}</h3>
       <p v-html="product.description"></p>
       <p>Prix unitaire: {{ product.unit_price }} €</p>
       <p>Quantité disponible: {{ product.quantity }}</p>
+    </div>
       <Rating :value="product.rating" />
     </div>
   </template>
