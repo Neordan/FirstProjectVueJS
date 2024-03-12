4032ba50f10c3fea0ecf05336287406587fe5e47
Neordan <melhian.adam@gmail.com>
2024-03-12 15:26:07 +0100

    Update Products.vue

--- src/components/Products.vue
+++ src/components/Products.vue
@@ -1,33 +1,37 @@
-<!-- Products.vue -->
-
 <template>
   <section>
     <h2>{{ products.length }} produits en vente</h2>
+    
     <section class="products">
-      <Product v-for="product in products" :key="product.id" :product="product" :cheapest-price="cheapestProducts" :product-class="productClass" />
+
+      <router-link v-for="product in products" :key="product.id" :to="{ name: 'productDetails', params: { id: product.id }}">
+        <Product  :product="product" :cheapest-price="cheapestProducts" :product-class="productClass" />
+      </router-link>
     </section>
   </section>
 </template>
 <script>
 import Product from './Product.vue';
+import Rating from './Rating.vue';
 import productsData from '../../data/products.json';
 
 export default {
   components: {
-    Product
+    Product,
+    Rating
   },
   data() {
     return {
+      name: 'Products',
       products: productsData,
       productClass: 'product'
     };
   },
   computed: {
+   //Obtenir le prix le moins cher parmi les produits
     cheapestProducts() {
-        return this.products.toSorted(function(a, b) {
-          return a.unit_price - b.unit_price;
-        })[0].unit_price;
+      return this.products.toSorted((a, b) => a.unit_price - b.unit_price)[0].unit_price;
     }
-  }
+  },
 };
 </script>
