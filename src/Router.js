48fc6647eb8d9f68b030c8235eed4cd30b86b761
Neordan <melhian.adam@gmail.com>
2024-03-12 09:31:36 +0100

    add path for routes

---
+++ src/Router.js
@@ -0,0 +1,49 @@
+import { createRouter, createWebHistory } from 'vue-router';
+import Home from './components/Home.vue';
+import Products from './components/Products.vue';
+import ProductDetails from './components/ProductDetails.vue';
+import Login from './components/Login.vue';
+import Contact from './components/Contact.vue';
+import ProductDetailsSimilar from './components/ProductDetailsSimilar.vue';
+
+
+const routes = [
+  {
+    path: '/',
+    name: 'home',
+    component: Home
+  },
+  {
+    path: '/products',
+    name: 'products',
+    component: Products
+  },
+  {
+    path: '/product/:id',
+    name: 'productDetails',
+    component: ProductDetails,
+    props: true
+  },
+{
+  path: '/similar-product/:id',
+  name: 'similarProductDetails',
+  component: ProductDetailsSimilar
+},
+  {
+    path: '/login',
+    name: 'login',
+    component: Login
+  },
+  {
+    path: '/contact',
+    name: 'contact',
+    component: Contact
+  }
+];
+
+const router = createRouter({
+  history: createWebHistory(),
+  routes
+});
+
+export default router;
