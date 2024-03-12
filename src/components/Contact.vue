26541dcf9fea06a4c00dfb72ac77143f0cbf1b66
Neordan <melhian.adam@gmail.com>
2024-03-12 09:31:12 +0100

    Add Contact component

---
+++ src/components/Contact.vue
@@ -0,0 +1,14 @@
+<!-- fais moi un composant contact -->
+<template>
+  <div>
+    <h1>Contact</h1>
+    <p>Vous pouvez nous contacter à l'adresse suivante :
+        <a href="mailto:ezrezrer@dsf.com">ezrezrer@dsf.com</a>
+    </p>
+  </div>
+</template>
+
+<script>
+export default {
+};
+</script>
