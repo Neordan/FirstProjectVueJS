a82d0a72be491207d479c21ef9575bbe2e4f7b15
Neordan <melhian.adam@gmail.com>
2024-03-12 09:30:45 +0100

    Style Enhancement

--- src/main.js
+++ src/main.js
@@ -1,6 +1,8 @@
 import '../src/assets/css/main.css';
 
-import { createApp } from 'vue'
-import App from './App.vue'
+import { createApp } from 'vue';
+import App from './App.vue';
+import router from './Router.js';
 
-createApp(App).mount('#app')
+createApp(App).use(router).mount('#app');
+
