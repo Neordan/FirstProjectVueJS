3d144f6ee66ef3f53865857388fbc66812638969
Neordan <melhian.adam@gmail.com>
2024-03-13 14:43:39 +0100

    Correction file

--- src/components/Products.vue
+++ src/components/Products.vue
@@ -1,34 +1,32 @@
 <template>
-  <section>
-    <h2>{{ products.length }} produits en vente</h2>
-    
-    <section class="products">
-
-      <router-link v-for="product in products" :key="product.id" :to="{ name: 'productDetails', params: { id: product.id }}">
-        <Product  :product="product" :cheapest-price="cheapestProducts" :product-class="productClass" />
-      </router-link>
+  <div>
+    <section>
+      <h2>{{ products.length }} produits en vente</h2>
+      <section class="products">
+        <router-link v-for="product in products" :key="product.id" :to="{ name: 'productDetails', params: { id: product.id }}">
+          <Product :product="product" :cheapest-price="cheapestProducts" :product-class="productClass" />
+        </router-link>
+      </section>
     </section>
-  </section>
+    <Cart :cart="cart" />
+  </div>
 </template>
+
 <script>
 import Product from './Product.vue';
-import Rating from './Rating.vue';
 import productsData from '../../data/products.json';
 
 export default {
   components: {
     Product,
-    Rating
   },
   data() {
     return {
-      name: 'Products',
       products: productsData,
-      productClass: 'product'
+      productClass: 'product',
     };
   },
   computed: {
-   //Obtenir le prix le moins cher parmi les produits
     cheapestProducts() {
       return this.products.toSorted((a, b) => a.unit_price - b.unit_price)[0].unit_price;
     }
