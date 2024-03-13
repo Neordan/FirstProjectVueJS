1b5ae2c5e3758df2ef9a97975258c2268f60848b
Neordan <melhian.adam@gmail.com>
2024-03-13 16:33:10 +0100

    adding a bubble to display the number of products in the basket

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,6 +1,3 @@
-
-<!-- header -->
-
 <template>
   <header>
       <nav>
@@ -8,10 +5,44 @@
           <li><router-link :to="{ name: 'home' }">Home</router-link></li>
           <li><router-link :to="{ name: 'products' }">Products</router-link></li>
           <img class="logo" src="../assets/img/logo2.png" alt="Vue Mastery" />
+          <li>
+            <router-link :to="{ name: 'cart' }">
+              <box-icon name='basket' color='#575757'></box-icon>
+              <!-- Bulle pour afficher le nombre d'articles dans le panier -->
+              <div class="cart-badge" v-if="cartItemCount > 0">{{ cartItemCount }}</div>
+            </router-link>
+          </li>
           <li><router-link :to="{ name: 'login' }">Connexion</router-link></li>
-          <li><router-link :to="{ name: 'cart' }"><box-icon name='basket' color='#575757' ></box-icon></router-link></li>
           <!-- <li><router-link :to="{ name: 'contact' }">Contact</router-link></li> -->
         </ul>
       </nav>
   </header>
 </template>
+
+<script>
+export default {
+  data() {
+    return {
+      cartItemCount: 0
+    };
+  },
+  created() {
+    // Charger le nombre d'articles dans le panier depuis le stockage local lors de la création du composant
+    const savedCart = localStorage.getItem('cart');
+    if (savedCart) {
+      const cart = JSON.parse(savedCart);
+      this.cartItemCount = cart.length;
+      
+    }
+  },
+  watch: {
+    // Surveiller les changements du panier et mettre à jour le nombre d'articles dans le panier
+    cart: {
+      handler(newCart) {
+        this.cartItemCount = newCart.length;
+      },
+      deep: true
+    }
+  }
+};
+</script>
