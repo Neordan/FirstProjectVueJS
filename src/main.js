31d8afbf040f99bfb4f60c75b10a3f9020be908b
Neordan <melhian.adam@gmail.com>
2024-03-15 11:30:53 +0100

    Update main.js

--- src/main.js
+++ src/main.js
@@ -2,7 +2,7 @@ import '../src/assets/css/main.css';
 
 import { createApp } from 'vue';
 import App from './App.vue';
-import router from './Router.js';
+import router from './router/Router.js';
 
 createApp(App).use(router).mount('#app');
 
